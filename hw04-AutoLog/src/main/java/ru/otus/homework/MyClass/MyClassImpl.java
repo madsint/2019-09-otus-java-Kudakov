5bff00ec305c66733e3cf63264a41377c290607c
madsint <52866738+madsint@users.noreply.github.com>
2019-12-29 22:15:17 +0300

    Update MyClassImpl.java

--- hw04-AutoLog/src/main/java/ru/otus/homework/MyClass/MyClassImpl.java
+++ hw04-AutoLog/src/main/java/ru/otus/homework/MyClass/MyClassImpl.java
@@ -1,22 +1,22 @@
 package ru.otus.homework.MyClass;
 
 public class MyClassImpl implements MyClassInterface {
-    @Override
+    @Log
     public void methodForLog(int a, int b) {
         System.out.println("method 1 for @Log");
     }
 
-    @Override
+    @Log
     public void methodForLog(String c) {
         System.out.println("method 2 for @Log");
     }
 
-    @Override
+    @Log
     public void methodForLog(int b) {
         System.out.println("method NOT for @Log");
     }
 
-    @Override
+    @Log
     public void method(int a) {
         System.out.println("method");
     }
